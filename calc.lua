1166765144783587d67f5b651f8efe855ee2241a
William Miller <Wmilleiv@gmail.com>
2025-01-23 13:32:42 -0500

    calc base

---
+++ calc.lua
@@ -0,0 +1,26 @@
+--TODO: Add functions for multiplication and division
+--TODO: call getInput for the operand and num2
+--TODO: Add a branching conditional to choose the right operator function
+--TODO: Call the functions to print the result
+--TODO: Add Error checking to make sure the inputs are numbers and operands
+
+
+--Call this function to add num1 and num2
+function add(num1, num2)
+    return num1+num2
+end
+
+--Call this function to subtract num2 from num1
+function sub(num1, num2)
+    return num1-num2
+end
+
+function getInput(valTypeString)
+    print("Enter a " ..  valTypeString .. ":")
+    return io.read()
+end
+
+--get the value for num1 from the user
+getInput("number")
+
+
