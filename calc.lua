d3bda60bd478a76c3039defca058442f91620665
William Miller <Wmilleiv@gmail.com>
2025-01-27 07:23:16 -0500

    comment cleanup

--- calc.lua
+++ calc.lua
@@ -1,4 +1,3 @@
---TODO: Add a branching conditional to choose the right operator function
 --TODO: Call the functions to print the result
 --TODO: Add Error checking to make sure the inputs are numbers and operands
 
