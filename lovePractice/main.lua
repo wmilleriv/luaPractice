fa8c12ac7769da50ce3be1a20a60289963f38f9e
William Miller <Wmilleiv@gmail.com>
2025-01-28 11:00:52 -0500

    tetris grid

--- lovePractice/main.lua
+++ lovePractice/main.lua
@@ -1,5 +1,17 @@
 function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
+    for y = 1, 18 do
+        for x = 1, 10 do
+            local blockSize = 20
+            local blockDrawSize =blockSize - 1
+            love.graphics.rectangle(
+                'fill',
+                (x - 1) * blockSize,
+                (y - 1) * blockSize,
+                blockDrawSize,
+                blockDrawSize
+            )
+        end
+    end
 end
 
 love.draw()
