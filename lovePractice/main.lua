7b07d237013a9fc70386ca96b8110cec310fd080
William Miller <Wmilleiv@gmail.com>
2025-01-29 13:45:28 -0500

    color test

--- lovePractice/main.lua
+++ lovePractice/main.lua
@@ -1,13 +1,46 @@
 function love.load()
     love.graphics.setBackgroundColor(255, 255, 255)
+
+    gridXCount = 10
+    gridYCount = 18
+
+    inert = {}
+    for y = 1, gridYCount do
+        inert[y] = {}
+        for x = 1, gridXCount do
+            inert[y][x] = ' '
+        end
+    end
+
+    -- Temporary
+    inert[18][1] = 'i'
+    inert[17][2] = 'j'
+    inert[16][3] = 'l'
+    inert[15][4] = 'o'
+    inert[14][5] = 's'
+    inert[13][6] = 't'
+    inert[12][7] = 'z'
 end
 
 function love.draw()
-    for y = 1, 18 do
-        for x = 1, 10 do
-            love.graphics.setColor(.87, .87, .87)
+    for y = 1, gridYCount do
+        for x = 1, gridXCount do
+            local colors = {
+                [' '] = {.87, .87, .87},
+                i = {.47, .76, .94},
+                j = {.93, .91, .42},
+                l = {.49, .85, .76},
+                o = {.92, .69, .47},
+                s = {.83, .54, .93},
+                t = {.97, .58, .77},
+                z = {.66, .83, .46},
+            }
+            local block = inert[y][x]
+            local color = colors[block] 
+            love.graphics.setColor(color)
+
             local blockSize = 20
-            local blockDrawSize =blockSize - 1
+            local blockDrawSize = blockSize - 1
             love.graphics.rectangle(
                 'fill',
                 (x - 1) * blockSize,
@@ -18,5 +51,3 @@ function love.draw()
         end
     end
 end
-
-love.draw()
