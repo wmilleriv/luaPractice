b92a968824e7344102cdfd4f3cfd3aa642c90c2c
William Miller <Wmilleiv@gmail.com>
2025-01-30 12:22:59 -0500

    refactor

--- lovePractice/main.lua
+++ lovePractice/main.lua
@@ -147,56 +147,41 @@ function love.load()
 end
 
 function love.draw()
+    local function drawBlock(block, x, y)
+         local colors = {
+            [' '] = {.87, .87, .87},
+            i = {.47, .76, .94},
+            j = {.93, .91, .42},
+            l = {.49, .85, .76},
+            o = {.92, .69, .47},
+            s = {.83, .54, .93},
+            t = {.97, .58, .77},
+            z = {.66, .83, .46},
+         }
+         local color = colors[block]
+         love.graphics.setColor(color)
+ 
+ 
+         local blockSize = 20
+         local blockDrawSize = blockSize -1
+         love.graphics.rectangle(
+             'fill',
+             (x-1) * blockSize,
+             (y-1) * blockSize,
+             blockDrawSize,
+             blockDrawSize
+         )
+    end
     for y = 1, gridYCount do
         for x = 1, gridXCount do
-            local colors = {
-                [' '] = {.87, .87, .87},
-                i = {.47, .76, .94},
-                j = {.93, .91, .42},
-                l = {.49, .85, .76},
-                o = {.92, .69, .47},
-                s = {.83, .54, .93},
-                t = {.97, .58, .77},
-                z = {.66, .83, .46},
-            }
-            local block = inert[y][x]
-            local color = colors[block] 
-            love.graphics.setColor(color)
-
-            local blockSize = 20
-            local blockDrawSize = blockSize - 1
-            love.graphics.rectangle(
-                'fill',
-                (x - 1) * blockSize,
-                (y - 1) * blockSize,
-                blockDrawSize,
-                blockDrawSize
-            )
+            drawBlock(inert[y][x], x, y)
         end
     end
 
     for y=1, 4 do
         for x = 1, 4 do
-            local block = pieceStructures[pieceType][pieceRotation][y][x]
             if block ~= ' ' then
-                local colors = {
-                    i = {.47, .76, .94},
-                    j = {.93, .91, .42},
-                    l = {.49, .85, .76},
-                    o = {.92, .69, .47},
-                    s = {.83, .54, .93},
-                    t = {.97, .58, .77},
-                    z = {.66, .83, .46},
-                }
-                local blockSize = 20
-                local blockDrawSize = blockSize -1
-                love.graphics.rectangle(
-                    'fill',
-                    (x-1) * blockSize,
-                    (y-1) * blockSize,
-                    blockDrawSize,
-                    blockDrawSize
-                )
+                drawBlock(block, x, y)
             end
         end
     end
