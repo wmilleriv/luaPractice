360046e8efdf9d8cd96b94fd7019bdf7d3726a9b
William Miller <Wmilleiv@gmail.com>
2025-02-20 13:41:02 -0500

    fizzbuzz

--- fizzBuzz.lua
+++ fizzBuzz.lua
@@ -1,6 +1,11 @@
 for i=1,100,1 do
+    if i%3 ~= 0 and i%5 ~= 0 then
+        print(i)
+    end
     if i%3 == 0 then
         print("fizz")
     end
-    print(i)
+    if i%5==0 then
+        print("buzz")
+    end
 end
